0669ca03f41125d6fbd1238f330f633471dde4ea
Sean Brown <inphomercial@gmail.com>
2015-06-13 15:21:50 -0600

    Reorganizing LightController, got some of the functionality working. Missing the guess LED logic

--- LightController/LightController.ino
+++ LightController/LightController.ino
@@ -1,68 +1,147 @@
-int first = 9;
-int second = 10;
-int third = 11;
+#include "Ai_WS2811.h"
 
-int redPin = 6;
-int greenPin = 5;
-int bluePin = 3;
+#define LED_DATA_PIN 8
+#define NUM_LEDS 10
 
-int buttonPin = 2;
-int buttonState = 0;
+#define P1_SELECT_BUTTON_PIN 7
+int P1_POSITION = 0;
+int P1_COLOR = 1;
+int p1ButtonState = 0;
+
+#define P2_SELECT_BUTTON_PIN 2
+int P2_POSITION = 0;
+int P2_COLOR = 9;
+int p2ButtonState = 0;
+
+// Global Middle Master Color LED
+#define GLOBAL_COLOR 5
+
+Ai_WS2811 ws2811;
+
+struct CRGB {
+  unsigned char g;
+  unsigned char r;
+  unsigned char b;
+} *leds;
 
 int lightState = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(first, OUTPUT);
-  pinMode(second, OUTPUT);
-  pinMode(third, OUTPUT);
+  ws2811.init(LED_DATA_PIN, NUM_LEDS);
+  
+  leds = (struct CRGB*)ws2811.getRGBData();
+  
+  // Initializes LEDS to 0
+  clearLeds();
+  
+  delay(2000);
+  
+  // Set global LED
+  randomizeGlobalLed();
+  
+  // Set starting position for players and light LED
+  initPlayerStartingPosition();
 
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+  pinMode(P1_SELECT_BUTTON_PIN, INPUT);
+  pinMode(P2_SELECT_BUTTON_PIN, INPUT);
+}
 
-  pinMode(buttonPin, INPUT);
+void clearLeds() {
+  for (int i=0; i<NUM_LEDS; ++i) {
+     setLedColor(i, 0, 0, 0);
+     ws2811.sendLedData();
+  }
+}
+
+void randomizeGlobalLed() {
+    int r = random(0, 255);  
+    int g = random(0, 255);
+    int b = random(0, 255);
+    
+    setLedColor(GLOBAL_COLOR, r, g, b);
+    ws2811.sendLedData();
+}
+
+void initPlayerStartingPosition() {
+    P1_POSITION = 4;
+    P2_POSITION = 6;
+    
+    setLedColor(P1_POSITION, 255, 0, 0);
+    setLedColor(P2_POSITION, 255, 0, 0);
+    ws2811.sendLedData();
+}
+
+void movePlayerPosition(int player) {
+  
+  if (player == 1) {
+    
+    setLedColor(P1_POSITION, 0, 0, 0);
+    
+    P1_POSITION--;
+    if(P1_POSITION <= 1) {
+      P1_POSITION = 4;
+    }
+    
+    if (P1_POSITION == 4) {
+      setLedColor(P1_POSITION, 255, 0, 0);  
+    }
+    
+    if (P1_POSITION == 3) {
+      setLedColor(P1_POSITION, 0, 255, 0);  
+    }
+    
+    if (P1_POSITION == 2) {
+      setLedColor(P1_POSITION, 0, 0, 255);  
+    }
+    
+  } else {
+    
+    setLedColor(P2_POSITION, 0, 0, 0);
+    
+    P2_POSITION++;
+    if(P2_POSITION >= 9) {
+      P2_POSITION = 6;
+    }
+    
+    if (P2_POSITION == 6) {
+      setLedColor(P2_POSITION, 255, 0, 0);  
+    }
+    
+    if (P2_POSITION == 7) {
+      setLedColor(P2_POSITION, 0, 255, 0);  
+    }
+    
+    if (P2_POSITION == 8) {
+      setLedColor(P2_POSITION, 0, 0, 255);  
+    }
+    
+  } 
+  
+  ws2811.sendLedData();
 }
 
 void loop() {
-
-    // Read the state of the pushbutton value
-    buttonState = digitalRead(buttonPin);
-
-    Serial.print(lightState);
-
-    if (buttonState == HIGH) {
+    
+    // Read the state of the player button values
+    p1ButtonState = digitalRead(P1_SELECT_BUTTON_PIN);
+    p2ButtonState = digitalRead(P2_SELECT_BUTTON_PIN);
+    
+    if (p1ButtonState == HIGH) {
       delay(1000);
-      lightState++;
+      movePlayerPosition(1);
     }
-
-    if (lightState == 1) {
-      digitalWrite(first, HIGH);
-      analogWrite(bluePin, 255);
-    }
-
-    if (lightState == 2) {
-      digitalWrite(second, HIGH);
-      analogWrite(redPin, 255);
-    }
-
-    if (lightState == 3) {
-      digitalWrite(third, HIGH);
-      analogWrite(greenPin, 255);
-    }
-
-    if (lightState >= 4) {
-      lightState = 0;
-      clear();
+    
+    if (p2ButtonState == HIGH) {
+      delay(1000);
+      movePlayerPosition(2);
     }
 }
 
-void clear() {
-    digitalWrite(first, LOW);
-    digitalWrite(second, LOW);
-    digitalWrite(third, LOW);
-
-    analogWrite(redPin, 0);
-    analogWrite(bluePin, 0);
-    analogWrite(greenPin, 0);
+void setLedColor(int led_index, int r, int g, int b) {
+  leds[led_index].r = constrain((int)255*r,0,255);
+  leds[led_index].g = constrain((int)255*g,0,255);
+  leds[led_index].b = constrain((int)255*b,0,255);
+  ws2811.sendLedData();
 }
+
